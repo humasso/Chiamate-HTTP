42a903b83173924d53335d86f65aed89b9414583
humasso <hussain.humas@iisgalvanimi.edu.it>
2020-11-10 08:25:31 +0000

    15.1

---
+++ applicazione/src/app/foo/foo.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.css']
+})
+export class FooComponent implements OnInit {
+  data: Object;
+  loading: boolean;
+  o : Observable<Object>;
+
+  constructor(public http: HttpClient) { }
+
+  makeRequest(): void {
+     this.loading = true;
+     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+     this.o.subscribe(this.getData);
+   }
+   getData = (d : Object) =>
+   {
+     this.data = d; //Notifico l’oggetto ricevuto dal server
+     this.loading = false;  // Notifico che ho ricevuto i dati
+   }
+
+   makeCompactRequest(): void {
+     this.loading = true;
+     this.http
+       .get('https://jsonplaceholder.typicode.com/posts/1')
+       .subscribe(data => {
+       this.data = data;
+       this.loading = false;
+       });
+      }
+
+    makeCompactPost(): void {
+    this.loading = true;
+    this.http
+     .post('https://jsonplaceholder.typicode.com/posts',
+       JSON.stringify({
+         body: 'bar',
+         title: 'foo',
+         userId: 1
+       })
+     )
+     .subscribe(data => {
+       this.data = data;
+       this.loading = false;
+     });
+ }
+
+  ngOnInit(): void {
+  }
+
+}
